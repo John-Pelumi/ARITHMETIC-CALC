c18f32bd59d066a2ada79752b6740f487a83a3e6
Your Name <you@example.com>
2022-06-23 03:39:03 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let x,y;
+x=parseInt(prompt("enter the first number"));
+y=parseInt(prompt("enter the second number"));
+let z = x+y;
+let a = x-y;
+let b = x*y;
+let c = x/y;
+
+alert("the sum of given numbers is:"+z);
+alert("the difference between given numbers is:"+a);
+alert("the product of given numbers is:"+b);
+alert("the quotient of given numbers is:"+c);
